c3b52e7bce5ac3516664eab4a1b61a67813f73ee
GeovannyTS <yowtecum@gmail.com>
2023-07-08 12:08:20 -0600

    Animaciones de carga

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'share-lazy-image',
+  templateUrl: './lazy-image.component.html',
+  styleUrls: ['./lazy-image.component.css']
+})
+export class LazyImageComponent implements OnInit {
+
+  @Input()
+  public url!:string;
+
+  @Input()
+  public alt:string = '';
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('URL is required');
+  }
+
+  onLoad(){
+    setTimeout(()=>{
+      this.hasLoaded = true;
+    })
+
+  }
+
+}
